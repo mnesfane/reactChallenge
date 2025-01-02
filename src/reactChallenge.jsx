1816ab9e403179d7177b5a8e0983454d45e2f0d7
mnesfane <abdessalamnesfane@gmail.com>
2025-01-02 17:23:59 +0100

    app file

--- src/reactChallenge.jsx
+++ src/reactChallenge.jsx
@@ -1,129 +1,71 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef} from 'react'
 
-// export function ReactChallenge(){
-//     const [mess, setMess] = useState('')
-//     const [indexButtonState, setIndexButton] = useState()
-//     let sameIndex = false
-//     const clavier = [
-//         ['0'],
-//         ['1'],                    
-//         ['2', 'a', 'b', 'c'],
-//         ['3', 'd', 'e', 'f'],     
-//         ['4', 'g', 'h', 'i'],     
-//         ['5', 'j', 'k', 'l'],     
-//         ['6', 'm', 'n', 'o'],     
-//         ['7', 'p', 'q', 'r', 's'],
-//         ['8', 't', 'u', 'v'],     
-//         ['9', 'w', 'x', 'y', 'z'],
-//         ['*'],                                       
-//         ['#']                   
-//     ];
-
-//     useEffect(()=>{
-//         if(indexButtonState !== undefined){
-//             setMess((prevMess) => prevMess + clavier[indexButtonState][0])
-//         }
-//     }, [indexButtonState])
-
-//     function handleClavier(indexButtonVar){
-//         setIndexButton((prevIndex)=>{
-//             if(prevIndex === indexButtonVar)
-//                 sameIndex = true
-//             else
-//                 sameIndex = false
-//             return indexButtonVar
-//         })
-//     }
-
-//     return(
-//         <>
-//         <table>
-//             <tbody>
-//                 <tr>
-//                     <td><button onClick={() => handleClavier(1)}>1</button></td>
-//                     <td><button onClick={() => handleClavier(2)}>2abc</button></td>
-//                     <td><button onClick={() => handleClavier(3)}>3def</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td><button onClick={() => handleClavier(4)}>4ghi</button></td>
-//                     <td><button onClick={() => handleClavier(5)}>5jkl</button></td>
-//                     <td><button onClick={() => handleClavier(6)}>6mno</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td><button onClick={() => handleClavier(7)}>7pqrs</button></td>
-//                     <td><button onClick={() => handleClavier(8)}>8tuv</button></td>
-//                     <td><button onClick={() => handleClavier(9)}>9wxyz</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td><button onClick={() => handleClavier(10)}>*</button></td>
-//                     <td><button onClick={() => handleClavier(0)}>0</button></td>
-//                     <td><button onClick={() => handleClavier(11)}>#</button></td>
-//                 </tr>
-//             </tbody>
-//         </table>
-//         <br />
-//         <div>{mess}</div>
-//         </>
-//     )
-// }
 export function ReactChallenge(){
+    const clavier = [
+    ['0'],
+    ['1'],                    
+    ['2', 'a', 'b', 'c'],
+    ['3', 'd', 'e', 'f'],
+    ['4', 'g', 'h', 'i'],     
+    ['5', 'j', 'k', 'l'],     
+    ['6', 'm', 'n', 'o'],     
+    ['7', 'p', 'q', 'r', 's'],
+    ['8', 't', 'u', 'v'],     
+    ['9', 'w', 'x', 'y', 'z'],
+    ['*'],                                       
+    ['#']                   
+];
     const [mess, setMess] = useState('')
     const [indexButtonState, setIndexButton] = useState()
-    let sameIndex = false
-    const clavier = [
-        ['0'],
-        ['1'],                    
-        ['2', 'a', 'b', 'c'],
-        ['3', 'd', 'e', 'f'],     
-        ['4', 'g', 'h', 'i'],     
-        ['5', 'j', 'k', 'l'],     
-        ['6', 'm', 'n', 'o'],     
-        ['7', 'p', 'q', 'r', 's'],
-        ['8', 't', 'u', 'v'],     
-        ['9', 'w', 'x', 'y', 'z'],
-        ['*'],                                       
-        ['#']                   
-    ];
+    let doubleClickIndex= useRef(0);
+    let interValId = useRef(null);
 
-    useEffect(()=>{
-        if(indexButtonState !== undefined){
-            setMess((prevMess) => prevMess + clavier[indexButtonState][0])
-        }
-    }, [indexButtonState])
-
-    function handleClavier(indexButtonVar){
-        setIndexButton((prevIndex)=>{
-            if(prevIndex === indexButtonVar)
-                sameIndex = true
-            else
-                sameIndex = false
-            return indexButtonVar
+    
+    const handleClavier = (indexButtonArg)=>{
+        // alert(indexButtonArg)
+        setIndexButton(prevIndexButton=>{
+            if(prevIndexButton === indexButtonArg && doubleClickIndex.current !== -1){
+                if(interValId.current) clearTimeout(interValId.current)
+                // alert(doubleClickIndex.current)
+                setMess((prevMess)=> prevMess.slice(0, -1))
+                doubleClickIndex.current === clavier[indexButtonArg].length - 1 ? doubleClickIndex.current = 0 : doubleClickIndex.current++
+                // alert(doubleClickIndex.current)
+            }
+            else{
+                doubleClickIndex.current = 0
+            }
+            setMess((prevMess)=>prevMess +  clavier[indexButtonArg][doubleClickIndex.current])
+            interValId.current = setTimeout(()=>{
+                doubleClickIndex.current = - 1
+            }, 1000)
+            return indexButtonArg
         })
     }
+
 
     return(
         <>
         <table>
             <tbody>
                 <tr>
-                    <td><button onClick={() => handleClavier(1)}>1</button></td>
-                    <td><button onClick={() => handleClavier(2)}>2abc</button></td>
-                    <td><button onClick={() => handleClavier(3)}>3def</button></td>
+                    <td><button onClick={() => {handleClavier(1), handleMessage()}}>1</button></td>
+                    <td><button onClick={() => {handleClavier(2)}}>2abc</button></td>
+                    <td><button onClick={() => {handleClavier(3), handleMessage()}}>3def</button></td>
                 </tr>
                 <tr>
-                    <td><button onClick={() => handleClavier(4)}>4ghi</button></td>
-                    <td><button onClick={() => handleClavier(5)}>5jkl</button></td>
-                    <td><button onClick={() => handleClavier(6)}>6mno</button></td>
+                    <td><button onClick={() => {handleClavier(4), handleMessage()}}>4ghi</button></td>
+                    <td><button onClick={() => {handleClavier(5), handleMessage()}}>5jkl</button></td>
+                    <td><button onClick={() => {handleClavier(6), handleMessage()}}>6mno</button></td>
                 </tr>
                 <tr>
-                    <td><button onClick={() => handleClavier(7)}>7pqrs</button></td>
-                    <td><button onClick={() => handleClavier(8)}>8tuv</button></td>
-                    <td><button onClick={() => handleClavier(9)}>9wxyz</button></td>
+                    <td><button onClick={() => {handleClavier(7), handleMessage()}}>7pqrs</button></td>
+                    <td><button onClick={() => {handleClavier(8), handleMessage()}}>8tuv</button></td>
+                    <td><button onClick={() => {handleClavier(9), handleMessage()}}>9wxyz</button></td>
                 </tr>
                 <tr>
-                    <td><button onClick={() => handleClavier(10)}>*</button></td>
-                    <td><button onClick={() => handleClavier(0)}>0</button></td>
-                    <td><button onClick={() => handleClavier(11)}>#</button></td>
+                    <td><button onClick={() => {handleClavier(10), handleMessage()}}>*</button></td>
+                    <td><button onClick={() => {handleClavier(0), handleMessage()}}>0</button></td>
+                    <td><button onClick={() => {handleClavier(11), handleMessage()}}>#</button></td>
                 </tr>
             </tbody>
         </table>
